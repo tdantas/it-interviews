10207e3fa0ccd573315ea9ae26cb0aa9e75d2141
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2014-06-16 15:49:40 +0100

    removing diff marks

--- matrix_prime_numbers/solutions/tdantas/ruby/solution.rb
+++ matrix_prime_numbers/solutions/tdantas/ruby/solution.rb
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-require './prime_matrix'
-require './line'
-=======
 $:.unshift File.dirname(__FILE__)
+
 require 'prime_matrix'
 require 'line'
-
->>>>>>> 19dde03075d200cd8246dde77385b6cc7fff42f7
 
 PrimeMatrix.new(10).print
