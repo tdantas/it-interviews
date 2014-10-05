63b81902da772e21bf42783b0e233888ad6cb301
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2014-10-05 18:19:38 +0100

    rename local variable to avoid conflict names

--- maze/solutions/tdantas/nodeJS/lib/event-dispatcher.js
+++ maze/solutions/tdantas/nodeJS/lib/event-dispatcher.js
@@ -3,17 +3,19 @@ module.exports = EventDispatcher();
 function EventDispatcher() {
   var next    = 1;
   var backlog = { };
+  var report  = [];
 
   return { process: process };
 
   function process(event) {
-    var event;
+    var curr;
     backlog[event.sequence] = event;
-
-    while(event = backlog[next]) {
+    
+    while((curr = backlog[next])) {
       delete backlog[next];
       next++;
-      event.process();
+
+      curr.process();
     }
   }
 }
