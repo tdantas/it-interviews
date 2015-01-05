d86c26402afced1b535337d25a9313ce31371c19
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2015-01-05 14:50:18 +0000

    getMin const time in elixir

---
+++ stack_data_structure_constant_time/solutions/tdantas/stack.ex
@@ -0,0 +1,24 @@
+defmodule Stack do
+  defstruct items: [], min: []
+
+  def push(element) do
+    %Stack{items: [element], min: [element] }
+  end
+
+  def push(%Stack{ items: [stack], min: [first | rest] }, element) do
+    newMin = Enum.min([first, element])
+    min =
+    %Stack{items: [element | stack], min: [newMin|[first|rest]]}
+  end
+
+  def pop(%Stack{items: [top | rest], min: [ _ | tail ] }) do
+    {top, %Stack{items: rest, min: tail }}
+  end
+
+  def getMin(stack) do
+    [ min| _ ]  = stack.min;
+    min
+  end
+
+end
+
